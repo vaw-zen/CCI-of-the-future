fe28ad74cd8ce88bbf745fdf25cc910f1bac5d4e
Ahmedchammakhii <117437634+Ahmedchammakhii@users.noreply.github.com>
2023-03-31 14:28:07 -0600

    main contact combined 2gether

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './form.css'
 import {BsArrowBarRight} from "react-icons/bs"
 const form = () => {
+  const [inputs,setinputs]=useState({sujet:"",nom:"",tel:"",email:"",object:""})
   return (
     <div className="form_container">
         <h1>Contactez-nous : </h1>
-        <input placeholder='Sujet : ' />
+        <input placeholder='Sujet : ' onChange={e=>setinputs(fields=>({...fields,sujet:e.target.value}))} />
         
-        <input placeholder='Votre nom' />
-        <input placeholder='Numero de telephone '  />
+        <input placeholder='Votre nom' onChange={e=>setinputs(fields=>({...fields,nom:e.target.value}))}  />
+        <input placeholder='Numero de telephone ' onChange={e=>setinputs(fields=>({...fields,tel:e.target.value}))}  />
         
-        <input placeholder='Votre email ' type={'email'} />
+        <input placeholder='Votre email ' type={'email'} onChange={e=>setinputs(fields=>({...fields,email:e.target.value}))}  />
         
-        <input placeholder='Sneak peak ' />
+        <input placeholder='Sneak peak ' onChange={e=>setinputs(fields=>({...fields,object:e.target.value}))}  />
         <button>Send it <BsArrowBarRight/> </button>
     </div>
 

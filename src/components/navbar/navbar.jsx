4617d3f2796c0fcc940d75fdd8928c1fc309729f
Ahmedchammakhii <117437634+Ahmedchammakhii@users.noreply.github.com>
2023-04-09 00:17:13 -0600

    services page done respons + ;enu

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,17 +5,23 @@ import React, { useState } from 'react'
 
 const navbar = ({current}) => {
     const [open,setOpen]=useState(false)
+    
+  const [menuActive, setMenuActive] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuActive(!menuActive);
+  };
   return (
    
-<div class="navbar">
-<div class="logo">
+<div className="navbar">
+<div className="logo">
     <p>Chaabane Cleaning int</p>
 </div>
-<div class="choices">
+<div className="choices">
     <p style={current==="Home" ? ({color:"#cafb42"}):{}}>Home</p>
     <div className="services-whole" onMouseOver={e=>setOpen(true)} onMouseLeave={e=>setOpen(false)}>
 
-    <div class="styled_choice">
+    <div className="styled_choice">
         <div></div>
           <p style={current==="Services" ? ({color:"#cafb42"}):{}}>Services</p>
 <IoIosArrowDown  onClick={e=>setOpen(open=>!open)}style={open ? {cursor:"pointer",transform:"rotate(180deg)",transition:"all .1s ease"} : {cursor:"pointer",transition:"all .1 ease"}}/>
@@ -32,18 +38,49 @@ const navbar = ({current}) => {
         <div className="triangle"></div></>}
 </div>
 </div>
-    <div class="styled_choice">
+    <div className="styled_choice">
        <div></div>
         <p style={current==="Projects" ? ({color:"#cafb42"}):{}}>Projects</p>
 
 </div>
 
-        <div class="styled_choice">
+        <div className="styled_choice">
            <div></div>
             <p>Contact Us</p>
 
         </div>
+
 </div>
+<a href="#" className={menuActive ? 'menuBtn act' : 'menuBtn'} onClick={handleMenuClick}>
+				<span class="lines"></span>
+			</a>
+			<nav className={menuActive ? 'mainMenu act' : 'mainMenu'}>
+				<ul>
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<p className="serv" onClick={e=>setOpen(open=>!open)}>Services</p>
+					</li>
+          {open && 
+          <>
+         <p className='btoline'>Entretien marbre</p>
+          <p className='btoline'>nettoyage aarterre</p>
+          <p className='btoline'>nettoyage moquette</p>
+          <p className='btoline'>nettoyage sallon</p>
+          <p className='btoline'>nettoyage voiture</p>
+          <p className='btoline'>nettoyage vitres
+            </p></>
+          }
+					<li>
+						<a href="/projects">Projects</a>
+					</li>
+					<li>
+						<a href="/contact">Contact</a>
+					</li>
+					
+				</ul>
+			</nav>
 </div>
   )  
 }

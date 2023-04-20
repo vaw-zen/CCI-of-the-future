de3b7c2689471635cfc3f1e99ba7e2e3623c3012
Med D. Bouthuri <med.d.bouthuri@gmail.com>
2023-04-20 08:35:41 +0100

    navbar inside layout

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,88 +1,96 @@
 
-import {IoIosArrowDown} from 'react-icons/io/index.js'
+import { IoIosArrowDown } from 'react-icons/io/index.js'
 import "./navbar.css"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const navbar = ({current}) => {
-    const [open,setOpen]=useState(false)
-    
+const navbar = () => {
+  const [open, setOpen] = useState(false)
   const [menuActive, setMenuActive] = useState(false);
+  const [current, setCurrentPath] = useState('');
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname.slice(0,9));
+  }, []);
 
   const handleMenuClick = () => {
     setMenuActive(!menuActive);
   };
+
+
+
   return (
-   
-<div className="navbar">
-<div className="logo">
-    <p>Chaabane Cleaning int</p>
-</div>
-<div className="choices">
-    <p style={current==="Home" ? ({color:"#cafb42",cursor:'pointer'}):{cursor:'pointer'}}>Home</p>
-    <div className="services-whole" onMouseOver={e=>setOpen(true)} onMouseLeave={e=>setOpen(false)}>
+    <div className="navbar">
+      <div className="logo">
+        <p>Chaabane Cleaning int</p>
+      </div>
+      <div className="choices">
+        <a href='/' style={current === "/" || "" ? ({ color: "#cafb42", cursor: 'pointer' }) : { cursor: 'pointer' }}>
+          Home
+        </a>
+        <div className="services-whole" onMouseOver={e => setOpen(true)} onMouseLeave={e => setOpen(false)}>
 
-    <div className="styled_choice">
-        <div></div>
-          <p style={current==="Services" ? ({color:"#cafb42"}):{}}>Services</p>
-<IoIosArrowDown  onClick={e=>setOpen(open=>!open)}style={open ? {cursor:"pointer",transform:"rotate(180deg)",transition:"all .1s ease"} : {cursor:"pointer",transition:"all .1 ease"}}/>
-{open && <>
-<div className="services_dropdown">
-            <p>Entretien marbre</p>
-            <p>nettoyage parterre</p>
-            <p>nettoyage moquette</p>
-            <p>nettoyage sallon</p>
-            <p>nettoyage voiture</p>
-            <p>nettoyage vitres
-            </p>
-            </div>
-        <div className="triangle"></div></>}
-</div>
-</div>
-    <div className="styled_choice">
-       <div></div>
-        <p style={current==="Projects" ? ({color:"#cafb42"}):{}}>Projects</p>
-
-</div>
-
+          <div className="styled_choice">
+            <div></div>
+            <p style={current === "/services" ? ({ color: "#cafb42" }) : {}}>Services</p>
+            <IoIosArrowDown onClick={e => setOpen(open => !open)} style={open ? { cursor: "pointer", transform: "rotate(180deg)", transition: "all .1s ease" } : { cursor: "pointer", transition: "all .1 ease" }} />
+            {open && <>
+              <div className="services_dropdown">
+                <p>Entretien marbre</p>
+                <p>nettoyage parterre</p>
+                <p>nettoyage moquette</p>
+                <p>nettoyage sallon</p>
+                <p>nettoyage voiture</p>
+                <p>nettoyage vitres
+                </p>
+              </div>
+              <div className="triangle"></div></>}
+          </div>
+        </div>
         <div className="styled_choice">
-           <div></div>
-            <p>Contact Us</p>
+          <div></div>
+          <p style={current === "/projects" ? ({ color: "#cafb42" }) : {}}>Projects</p>
 
         </div>
 
-</div>
-<a href="#" className={menuActive ? 'menuBtn act' : 'menuBtn'} onClick={handleMenuClick}>
-				<span className="lines"></span>
-			</a>
-			<nav className={menuActive ? 'mainMenu act' : 'mainMenu'}>
-				<ul>
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<p className="serv" onClick={e=>setOpen(open=>!open)}>Services</p>
-					</li>
-          {open && 
-          <>
-         <p className='btoline'>Entretien marbre</p>
-          <p className='btoline'>nettoyage aarterre</p>
-          <p className='btoline'>nettoyage moquette</p>
-          <p className='btoline'>nettoyage sallon</p>
-          <p className='btoline'>nettoyage voiture</p>
-          <p className='btoline'>nettoyage vitres
-            </p></>
+        <div className="styled_choice">
+          <div></div>
+          <p>Contact Us</p>
+
+        </div>
+
+      </div>
+      <a href="#" className={menuActive ? 'menuBtn act' : 'menuBtn'} onClick={handleMenuClick}>
+        <span className="lines"></span>
+      </a>
+      <nav className={menuActive ? 'mainMenu act' : 'mainMenu'}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <p className="serv" onClick={e => setOpen(open => !open)}>Services</p>
+          </li>
+          {open &&
+            <>
+              <p className='btoline'>Entretien marbre</p>
+              <p className='btoline'>nettoyage aarterre</p>
+              <p className='btoline'>nettoyage moquette</p>
+              <p className='btoline'>nettoyage sallon</p>
+              <p className='btoline'>nettoyage voiture</p>
+              <p className='btoline'>nettoyage vitres
+              </p></>
           }
-					<li>
-						<a href="/projects">Projects</a>
-					</li>
-					<li>
-						<a href="/contact">Contact</a>
-					</li>
-					
-				</ul>
-			</nav>
-</div>
-  )  
+          <li>
+            <a href="/projects">Projects</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default navbar

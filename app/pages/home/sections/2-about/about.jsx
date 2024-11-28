f68322564e3a4d022e6ad1441d353d3bf79133a8
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-28 17:19:53 +0100

    about section tablet UI

--- app/pages/home/sections/2-about/about.jsx
+++ app/pages/home/sections/2-about/about.jsx
@@ -3,28 +3,25 @@ import React from 'react'
 import content from './about.json'
 import styles from './about.module.css'
 import Image from 'next/image'
+import { ArrowUpRight } from 'lucide-react'
 
 export default function About() {
     return (
         <section className={styles.section}>
-            <BlurBox top={[0]} left={['82.5vw']} />
+            <BlurBox top={[0, '5vw']} left={['82.5vw', '95vw']} />
             <div className={styles.flexContainer}>
                 <Image
-                    src="/home/about.png"
+                    src={content.img}
                     alt="about"
                     width={0}
                     height={0}
                     sizes="50vw"
-                    style={{
-                        width: '100%',
-                        height: '32.916vw',
-                        objectFit: 'cover'
-                    }}
+                    className={styles.image}
                 />
             </div>
             <div className={styles.contentContainer}>
-                <h1 className={styles.slug}>{content.slug}</h1>
-                <h2 className={styles.highlight}>{content.highlight}</h2>
+                <h2 className={styles.slug}>{content.slug}</h2>
+                <h3 className={styles.highlight}>{content.highlight}</h3>
                 <p className={styles.paragraph}>{content.paragraph}</p>
                 <ul className={styles.list}>
                     {content.list.map((text, index) => (
@@ -34,6 +31,9 @@ export default function About() {
                         </li>
                     ))}
                 </ul>
+                <div className={styles.circleContainer}>
+                    <ArrowUpRight className={styles.arrow} />
+                </div>
             </div>
         </section>
     )

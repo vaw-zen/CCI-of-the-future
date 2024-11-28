860b512531db737cb96523386cceafd3dbe34ca3
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-28 15:43:52 +0100

    cms approach

--- app/libs/components/BlurBox/BlurBox.jsx
+++ app/libs/components/BlurBox/BlurBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function BlurBox() {
+export default function BlurBox({ top, left }) {
     return <div style={{
-        backgroundColor: 'var(--ac-primary)', width: '2px', height: '2px', position: 'absolute', left: '50vw', top: '20vw',
-        boxShadow: '0 0 5vw 7vw var(--ac-primary)', opacity: 0.2
+        backgroundColor: 'var(--ac-primary)', width: '2px', height: '2px', position: 'absolute', left: left[0], top: top[0],
+        boxShadow: '0 0 5.75vw 7.3vw var(--ac-primary)', opacity: 0.2, zIndex:-1
     }}>
 
     </div>
--- app/pages/home/home.jsx
+++ app/pages/home/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Hero from './sections/1-hero/hero'
+import About from './sections/2-about/about'
 
 export default function Home() {
     return <main style={{ height: '300vh' }}>
         <Hero />
+        <About />
     </main>
 }

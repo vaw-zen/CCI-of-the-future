c731c0efdcd4bfa5d1fe4c6e5cb11cb78e38ae9f
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-28 19:50:48 +0100

    services UI

--- app/pages/home/sections/2-about/about.jsx
+++ app/pages/home/sections/2-about/about.jsx
@@ -2,26 +2,27 @@ import BlurBox from '@/app/libs/components/BlurBox/BlurBox'
 import React from 'react'
 import content from './about.json'
 import styles from './about.module.css'
+import pageStyles from '../../home.module.css'
 import Image from 'next/image'
 import { ArrowUpRight } from 'lucide-react'
 
 export default function About() {
     return (
         <section className={styles.section}>
-            <BlurBox top={[0, '5vw', '5vw']} left={['82.5vw', '95vw', '95vw']} />
+            <BlurBox className={styles.BlurBox} />
             <div className={styles.flexContainer}>
                 <Image
                     src={content.img}
-                    alt="about"
+                    alt="about us"
                     width={0}
                     height={0}
-                    sizes="50vw"
+                    sizes="(max-width: 768px) 100vw, 50vw"
                     className={styles.image}
                 />
             </div>
             <div className={styles.contentContainer}>
-                <h2 className={styles.slug}>{content.slug}</h2>
-                <h3 className={styles.highlight}>{content.highlight}</h3>
+                <h2 className={pageStyles.slug}>{content.slug}</h2>
+                <h3 className={pageStyles.highlight}>{content.highlight}</h3>
                 <p className={styles.paragraph}>{content.paragraph}</p>
                 <ul className={styles.list}>
                     {content.list.map((text, index) => (

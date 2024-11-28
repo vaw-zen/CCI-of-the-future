50a966af357fff63e0aefe212d4dcea6bcfdc8bc
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-28 17:19:53 +0100

    about section tablet UI

--- app/libs/components/BlurBox/BlurBox.jsx
+++ app/libs/components/BlurBox/BlurBox.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
 export default function BlurBox({ top, left }) {
-    return <div style={{
-        backgroundColor: 'var(--ac-primary)', width: '2px', height: '2px', position: 'absolute', left: left[0], top: top[0],
-        boxShadow: '0 0 5.75vw 7.3vw var(--ac-primary)', opacity: 0.2, zIndex:-1
-    }}>
+    return (
+        <>
+            <div className='blurBox' />
 
-    </div>
+            <style>{`
+                .blurBox {
+                    top: ${top[0]}; 
+                    left: ${left[0]}; 
+                    background-color: var(--ac-primary); 
+                    width: 2px; 
+                    height: 2px; 
+                    position: absolute; 
+                    box-shadow: 0 0 5.75vw 7.3vw var(--ac-primary); 
+                    opacity: 0.2; 
+                    z-index: -1;
+                }
+
+                @media (max-width: 1024px) {
+                    .blurBox {
+                        top: ${top[1]};
+                        left: ${left[1]};
+                        box-shadow: 0 0 17.5vw 16vw var(--ac-primary); 
+                    }
+                }
+
+                @media (max-width: 768px) {
+                    .blurBox {
+                        top: ${top[2]};
+                        left: ${left[2]};
+                    }
+                }
+            `}</style>
+        </>
+    )
 }

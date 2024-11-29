39a3fc0f7fe238d85d051804a880b6ccf6d141dc
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-29 10:27:17 +0100

    more light weigh icons

---
+++ app/libs/components/icons.jsx
@@ -0,0 +1,7 @@
+export function IconoirArrowUpRight(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M6 19L19 6m0 0v12.48M19 6H6.52"></path></svg>);
+}
+
+export function RadixIconsCaretRight(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15" {...props}><path fill="currentColor" fillRule="evenodd" d="M6.182 4.182a.45.45 0 0 1 .636 0l3 3a.45.45 0 0 1 0 .636l-3 3a.45.45 0 1 1-.636-.636L8.864 7.5L6.182 4.818a.45.45 0 0 1 0-.636" clipRule="evenodd"></path></svg>);
+}

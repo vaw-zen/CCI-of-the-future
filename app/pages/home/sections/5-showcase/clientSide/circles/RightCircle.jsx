86b688f0c623496dcfa86879779ff082d6f6c7a5
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-11-29 14:49:19 +0100

    right circle UI & MI

--- app/pages/home/sections/5-showcase/clientSide/circles/RightCircle.jsx
+++ app/pages/home/sections/5-showcase/clientSide/circles/RightCircle.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import styles from '../../showcase.module.css'
 
 export default function RightCircle({ circle }) {
     return (
-        <div style={{ width: '100%', height: '100%', position: 'relative', borderRadius: '50%' }}>
+        <>
+            <div className={styles.shadow} />
             <div style={{
-                width: '100%', height: '100%', background: 'var(--ac-primary)', position: 'absolute', left: 0, top: 0,
-                boxShadow: '0px 0px 5px 4px rgba(203,251,66,0.75)', borderRadius: '50%', transform: 'scale(.95)'
-            }} />
+                background: 'var(--bg-base)', border: '1px solid rgb(51, 51, 53)', width: '100%', height: '100%', position: 'relative', zIndex: 1, borderRadius: '50%',
+                display: 'flex', display: 'flex', alignItems: 'center', gap: '.5vw', justifyContent: 'center',
+            }}>
 
-            <div style={{ background: 'var(--bg-base)', border: '1px solid rgb(51, 51, 53)', width: '100%', height: '100%', position: 'relative', zIndex: 1, borderRadius: '50%' }}>
-
+                <div style={{ width: '7vw', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    {circle.map(element => <h2 style={{
+                        height: '5vw', display: 'flex', justifyContent: 'center', alignItems: 'center',
+                        fontSize: '3.334vw', width: '100%', color: 'var(--ac-primary)'
+                    }}>{element.number}</h2>)}
+                </div>
+                <div style={{ width: '5vw', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    {circle.map(element => <p style={{
+                        height: '5vw', display: 'flex', justifyContent: 'center', alignItems: 'center',
+                        fontSize: '.9vw', color: 'var(--t-thri)'
+                    }}>{element.desc}</p>)}
+                </div>
             </div>
-        </div>
+        </>
     )
 }

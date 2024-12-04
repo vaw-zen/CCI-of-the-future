a46ba9be144fc91af3397d5c7033e94bceaa7327
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-04 22:50:16 +0100

    projects mobile UI

--- app/pages/home/sections/6-projects/project.jsx
+++ app/pages/home/sections/6-projects/project.jsx
@@ -8,18 +8,52 @@ export default function Project() {
     const [firstArticle] = content.articles
 
     return (
-        <section>
-            <div className={styles.projects}>
-                <div className={styles.mainColumn}>
-                    <h2 className={pageStyles.slug}>{content.slug}</h2>
-                    <h3 className={pageStyles.highlight}>{content.highlight}</h3>
-                    <div className={styles.imageContainer}>
-                        <img className={styles.image} src={firstArticle.img} />
+        <section className={styles.projects}>
+            <div className={styles.mainColumn}>
+                <h2 className={pageStyles.slug}>{content.slug}</h2>
+                <h3 className={pageStyles.highlight}>{content.highlight}</h3>
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src={firstArticle.img} />
+                    <div className={styles.overlay}>
+                        <div className={styles.contentWrapper}>
+                            <Link href={firstArticle.link} className={styles.title}>{firstArticle.title}</Link>
+                            <p className={styles.tags}>
+                                {firstArticle.tags.map((tag, index) => (
+                                    <Link href={tag.link} key={index}>{tag.name},</Link>
+                                ))}
+                            </p>
+                        </div>
+                        <div className={styles.buttonWrapper}>
+                            <button className={styles.button}>
+                                <MdiArrowTopRightThin className={styles.icon} />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <button className={styles.showProjects}>
+                    <div className={styles.spinContainer}>
+                        {Array.from({ length: 15 }, (ç, index) => (
+                            <div
+                                key={index}
+                                className={styles.line}
+                                style={{ transform: `translate(-50%, -50%) rotate(${15 * index}deg)` }}
+                            />
+                        ))}
+                    </div>
+                    <div className={styles.innerButton}>
+                        <h2 className={styles.text}>voir tout les projets</h2>
+                    </div>
+                </button>
+            </div>
+            <div className={styles.articleGrid}>
+                {content.articles.map((article, i) => !i ? null : (
+                    <div className={styles.articleContainer} key={i}>
+                        <img className={styles.articleImage} src={article.img} />
                         <div className={styles.overlay}>
                             <div className={styles.contentWrapper}>
-                                <Link href={firstArticle.link} className={styles.title}>{firstArticle.title}</Link>
+                                <Link href={article.link} className={styles.title}>{article.title}</Link>
                                 <p className={styles.tags}>
-                                    {firstArticle.tags.map((tag, index) => (
+                                    {article.tags.map((tag, index) => (
                                         <Link href={tag.link} key={index}>{tag.name},</Link>
                                     ))}
                                 </p>
@@ -31,45 +65,10 @@ export default function Project() {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.articleGrid}>
-                    {content.articles.map((article, i) => !i ? null : (
-                        <div className={styles.articleContainer} key={i}>
-                            <img className={styles.articleImage} src={article.img} />
-                            <div className={styles.overlay}>
-                                <div className={styles.contentWrapper}>
-                                    <Link href={article.link} className={styles.title}>{article.title}</Link>
-                                    <p className={styles.tags}>
-                                        {article.tags.map((tag, index) => (
-                                            <Link href={tag.link} key={index}>{tag.name},</Link>
-                                        ))}
-                                    </p>
-                                </div>
-                                <div className={styles.buttonWrapper}>
-                                    <button className={styles.button}>
-                                        <MdiArrowTopRightThin className={styles.icon} />
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                ))}
             </div>
 
-            <button className={styles.showProjects}>
-                <div className={styles.spinContainer}>
-                    {Array.from({ length: 15 }, (ç, index) => (
-                        <div
-                            key={index}
-                            className={styles.line}
-                            style={{ transform: `translate(-50%, -50%) rotate(${15 * index}deg)` }}
-                        />
-                    ))}
-                </div>
-                <div className={styles.innerButton}>
-                    <h2 className={styles.text}>voir tout les projets</h2>
-                </div>
-            </button>
+
         </section>
     )
 }

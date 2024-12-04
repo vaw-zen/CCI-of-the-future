dea8cd45874bc625af1d692b4fcc40df67289825
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-04 21:51:42 +0100

    projects desktop UI

--- app/libs/components/icons.jsx
+++ app/libs/components/icons.jsx
@@ -5,3 +5,7 @@ export function IconoirArrowUpRight(props) {
 export function RadixIconsCaretRight(props) {
 	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15" {...props}><path fill="currentColor" fillRule="evenodd" d="M6.182 4.182a.45.45 0 0 1 .636 0l3 3a.45.45 0 0 1 0 .636l-3 3a.45.45 0 1 1-.636-.636L8.864 7.5L6.182 4.818a.45.45 0 0 1 0-.636" clipRule="evenodd"></path></svg>);
 }
+
+export function MdiArrowTopRightThin(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m11.93 5l2.83 2.83L5 17.59L6.42 19l9.76-9.75L19 12.07V5z"></path></svg>);
+}
--- app/pages/home/sections/6-projects/project.jsx
+++ app/pages/home/sections/6-projects/project.jsx
@@ -1,19 +1,75 @@
 import content from './projects.json'
 import pageStyles from '../../home.module.css'
+import styles from './project.module.css'
+import { MdiArrowTopRightThin } from '@/app/libs/components/icons'
+import Link from 'next/link'
 
 export default function Project() {
-    return <section style={{ position: 'relative', marginTop: '7.5vw', display: 'flex', gap: '1.5625vw' }}>
-        <div style={{ width: '38.5848vw' }}>
-            <h2 className={pageStyles.slug}>{content.slug}</h2>
-            <h3 className={pageStyles.highlight}>{content.highlight}</h3>
-            <div style={{ height: '28.9584vw', width: '100%', display: 'flex', marginTop: '3vw', overflow: 'hidden' }}>
-                <img style={{ flex: 1, objectFit: 'cover' }} src={content.lefSide.img} />
-            </div>
-        </div>
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '1.5625vw', marginTop: '3vw' }}>
-            <img style={{ width: '100%', objectFit: 'cover', height: '20vw' }} src='/home/about.png' />
-            <img style={{ width: '100%', objectFit: 'cover', height: '20vw' }} src='/home/about.png' />
+    const [firstArticle] = content.articles
 
-        </div>
-    </section>
+    return (
+        <section>
+            <div className={styles.projects}>
+                <div className={styles.mainColumn}>
+                    <h2 className={pageStyles.slug}>{content.slug}</h2>
+                    <h3 className={pageStyles.highlight}>{content.highlight}</h3>
+                    <div className={styles.imageContainer}>
+                        <img className={styles.image} src={firstArticle.img} />
+                        <div className={styles.overlay}>
+                            <div className={styles.contentWrapper}>
+                                <Link href={firstArticle.link} className={styles.title}>{firstArticle.title}</Link>
+                                <p className={styles.tags}>
+                                    {firstArticle.tags.map((tag, index) => (
+                                        <Link href={tag.link} key={index}>{tag.name},</Link>
+                                    ))}
+                                </p>
+                            </div>
+                            <div className={styles.buttonWrapper}>
+                                <button className={styles.button}>
+                                    <MdiArrowTopRightThin className={styles.icon} />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.articleGrid}>
+                    {content.articles.map((article, i) => !i ? null : (
+                        <div className={styles.articleContainer} key={i}>
+                            <img className={styles.articleImage} src={article.img} />
+                            <div className={styles.overlay}>
+                                <div className={styles.contentWrapper}>
+                                    <Link href={article.link} className={styles.title}>{article.title}</Link>
+                                    <p className={styles.tags}>
+                                        {article.tags.map((tag, index) => (
+                                            <Link href={tag.link} key={index}>{tag.name},</Link>
+                                        ))}
+                                    </p>
+                                </div>
+                                <div className={styles.buttonWrapper}>
+                                    <button className={styles.button}>
+                                        <MdiArrowTopRightThin className={styles.icon} />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <button className={styles.showProjects}>
+                <div className={styles.spinContainer}>
+                    {Array.from({ length: 15 }, (ç, index) => (
+                        <div
+                            key={index}
+                            className={styles.line}
+                            style={{ transform: `translate(-50%, -50%) rotate(${15 * index}deg)` }}
+                        />
+                    ))}
+                </div>
+                <div className={styles.innerButton}>
+                    <h2 className={styles.text}>voir tout les projets</h2>
+                </div>
+            </button>
+        </section>
+    )
 }

9cc24b006bd1b6111bed1afeba5aa4733bbc02b1
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-05 11:53:41 +0100

    grid UI

--- app/pages/home/sections/7-clients/clients.jsx
+++ app/pages/home/sections/7-clients/clients.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './clients.module.css'
 
 export default function Clients() {
   return (
-    <section>
-        
+    <section className={styles.clientsGrid}>
+      {Array.from({ length: 8 }, (_, index) => (
+        <div key={index} className={styles.clientCell} />
+      ))}
     </section>
   )
 }

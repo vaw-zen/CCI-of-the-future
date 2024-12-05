91546d20e0e990b46039b8ea39f9583b45b5d176
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-05 13:39:06 +0100

    clients Desktop UI

--- app/libs/components/icons.jsx
+++ app/libs/components/icons.jsx
@@ -9,3 +9,7 @@ export function RadixIconsCaretRight(props) {
 export function MdiArrowTopRightThin(props) {
 	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="m11.93 5l2.83 2.83L5 17.59L6.42 19l9.76-9.75L19 12.07V5z"></path></svg>);
 }
+
+export function UilArrowRight(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M17.92 11.62a1 1 0 0 0-.21-.33l-5-5a1 1 0 0 0-1.42 1.42l3.3 3.29H7a1 1 0 0 0 0 2h7.59l-3.3 3.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l5-5a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76"></path></svg>);
+}

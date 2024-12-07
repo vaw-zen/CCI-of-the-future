189b4d4f34e3a9d1f614dda6068ded0dc065bbab
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-08 00:27:44 +0100

    header UI

--- app/pages/home/sections/3-services/services.func.js
+++ app/pages/home/sections/3-services/services.func.js
@@ -4,22 +4,22 @@ import styles from './services.module.css'
 import { useEffect } from 'react';
 
 export function servicesSI() {
+
     useEffect(() => {
-
-        const cards = Array.from(document.getElementsByClassName(styles.serviceCard))
-        cards.forEach((element, index) => {
-            scrollTrigger.createInstance('service-card' + index, element, {
-                values: [[100, 0]],
-                startPoint: [0],
-                endPoint: [0.9],
-                callback: ({ v, vw }) => {
-                    element.style.transform = `translate3d(${v[0]}%, 0, 0)`
-                }
+            const cards = Array.from(document.getElementsByClassName(styles.serviceCard))
+            cards.forEach((element, index) => {
+                scrollTrigger.createInstance('service-card' + index, element, {
+                    values: [[100, 0]],
+                    startPoint: [0],
+                    endPoint: [0.9],
+                    callback: ({ v, vw }) => {
+                        element.style.transform = `translate3d(${v[0]}%, 0, 0)`
+                    }
+                });
             });
-        });
 
-        return () => {
-            cards.forEach((element, index) => scrollTrigger.removeInstance('service-card' + index));
-        };
+            return () => {
+                cards.forEach((_, index) => scrollTrigger.removeInstance('service-card' + index));
+            };
     }, [])
 }

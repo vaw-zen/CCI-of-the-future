66a771fb856e681e4b886d746dd6613f7181365d
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-07 16:43:11 +0100

    services SI

--- app/layout/header/header.jsx
+++ app/layout/header/header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import styles from './header.module.css'
 
 export default function Header() {
-    return (
-        <nav style={{ width: '100%', height: '5.2084vw', background: 'brown' }}>
+    return <>
+        <nav className={styles.nav} style={{ width: '100%', height: '5.2084vw', background: 'brown', position: 'fixed', top: 0, left: 0, zIndex:99999 }}>
 
         </nav>
-    )
+        <div style={{ marginTop: '5.2084vw' }} />
+    </>
 }
---
+++ app/pages/home/home.func.js
@@ -0,0 +1,5 @@
+import { servicesSI } from "./sections/3-services/services.func";
+
+export function homeScrollTriggers() {
+    servicesSI()
+}

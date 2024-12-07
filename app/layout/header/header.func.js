c5b1e53497f3f84d6d11e738213085fe3be5dcf0
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-07 18:03:59 +0100

    header SI

--- app/layout/header/header.func.js
+++ app/layout/header/header.func.js
@@ -1,17 +1,43 @@
+import { createRef } from 'react';
 import styles from './header.module.css'
-export function headerSI() {
-    // const nav = document.querySelector('.' + styles.nav)
-    // scrollInteraction.createInstance('about-entrance', element, {
-    //     values: [[0.1, 1], [35, 65]],
-    //     startPoint: [0.5],
-    //     endPoint: [0.15, 1],
-    //     isUnrestricted: true,
-    //     callback: (v) => {
-    //         bodyBG(body, v[0], true, true, () => {
-    //             if (body.style.background !== projects_data[projects_data.length - 1].background) {
-    //                 body.style.background = projects_data[projects_data.length - 1].background;
-    //             }
-    //         });
-    //     }
-    // });
+import { throttle } from '@/app/libs/vz/utils';
+
+
+
+
+const prevScroll = createRef(0)
+const direction = createRef('down')
+
+export function headerScrollInteraction() {
+    const nav = document.querySelector('.' + styles.nav)
+    if (!nav) return; // Safety check
+
+    if (scrollY <= nav.clientHeight) {
+        if ( nav.style.transform !== 'translateY(0px)') {
+            direction.current = 'up'
+            nav.style.transform = 'translateY(0px)'
+    
+        }
+    } else {
+        if (!direction.current) {
+            nav.style.transform = 'translateY(-100%)'
+            direction.current = 'down'
+        } else {
+            if (prevScroll.current < scrollY) {
+                if (direction.current === 'up') {
+                    nav.style.transform = 'translateY(-100%)'
+                }
+                direction.current = 'down'
+            } else if (prevScroll.current > scrollY) {
+                if (direction.current === 'down') {
+                    nav.style.transform = 'translateY(0px)'
+                }
+                direction.current = 'up'
+            }
+        }
+    }
+    prevScroll.current = scrollY
 }
+
+// Throttled version
+export const headerSI = throttle(headerScrollInteraction, 100); // 100ms throttle
--- app/layout/header/header.jsx
+++ app/layout/header/header.jsx
@@ -2,9 +2,8 @@ import styles from './header.module.css'
 
 export default function Header() {
     return <>
-        <nav className={styles.nav} style={{ width: '100%', height: '5.2084vw', background: 'brown', position: 'fixed', top: 0, left: 0, zIndex:99999 }}>
+        <nav className={styles.nav} style={{ width: '100%', height: '5.2084vw', background: 'brown', position: 'fixed', top: 0, left: 0, zIndex: 99999, transition: 'transform .5s cubic-bezier(0.165,0.840,0.440,1.000)' }}>
 
         </nav>
-        <div style={{ marginTop: '5.2084vw' }} />
     </>
 }

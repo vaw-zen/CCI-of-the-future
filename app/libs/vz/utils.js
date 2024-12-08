d9e9a3b526aadf966ef93c2ac1ed3a7dc07b0165
Med D. Bouthouri <med.d.bouthuri@gmail.com>
2024-12-08 21:06:16 +0100

    parralax on video button

--- app/libs/vz/utils.js
+++ app/libs/vz/utils.js
@@ -1,12 +1,27 @@
 export function throttle(func, limit) {
-    let inThrottle;
-    return function() {
-      const args = arguments;
-      const context = this;
-      if (!inThrottle) {
-        func.apply(context, args);
-        inThrottle = true;
-        setTimeout(() => inThrottle = false, limit);
-      }
+  let inThrottle;
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
     }
   }
+}
+
+export function addTransitionEnd(element, func, cancel, repeat) {
+  function handleTransitionEnd(event) {
+    func(event)
+    if (!repeat) { element.removeEventListener('transitionend', handleTransitionEnd); }
+    if (cancel) { element.removeEventListener('transitioncancel', handleTransitionEnd); }
+  }
+  element.addEventListener('transitionend', handleTransitionEnd);
+  if (cancel) { element.addEventListener('transitioncancel', handleTransitionEnd); }
+}
+export function foreach(arr, f, end, start) {
+  for (let i = (start || 0); (end || arr.length) > i; i++) {
+    f(arr[i], i)
+  }
+}

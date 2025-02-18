0feeb7d121e9d3846b1cd633edbba8d07650a908
ChaabaneFares <chaaben.fares94@gmail.com>
2025-02-18 18:47:29 +0100

    header adjustment

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -4,7 +4,7 @@ import HeroHeader from '@/utils/components/reusableHeader/HeroHeader'
 
 export default function page() {
     return (
-        <main style={{ width: '100%', paddingTop: '5vw', paddingBottom: '50vw' }}>
+        <main style={{ width: '100%', paddingBottom: '50vw' }}>
             <HeroHeader title={'Services'} />
             <Services />
         </main>

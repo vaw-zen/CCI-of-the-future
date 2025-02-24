a37b137a8bfac515fd8270c968fb721bfb9e7b56
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-02-24 22:10:48 +0100

    details: responsive ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={dmSans.className}>
+    <html lang="fr" className={dmSans.className}>
       <Initializer />
 
       <body>

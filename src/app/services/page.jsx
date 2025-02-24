600d4d9a0e0ef4fb5c03784f12c8aaeb57a1a24d
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-02-24 20:19:59 +0100

    details UI

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Services from './sections/2-services/services'
 import HeroHeader from '@/utils/components/reusableHeader/HeroHeader'
+import Details from './sections/1-details/details'
 
 export default function page() {
     return (
-        <main style={{ width: '100%', paddingBottom: '50vw' }}>
+        <main style={{ width: '100%', paddingBottom: '0' }}>
             <HeroHeader title={'Services'} />
+            <Details />
             <Services />
         </main>
     )

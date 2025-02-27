3f88f7ce15fdec81cb58363119395e810113c2d4
Med Dhia Bouthouri <dhia.bouthouri@atlasenergies.fr>
2025-02-27 19:19:08 +0100

    fix scroll animations when swapping from page to page

--- src/app/home/sections/3-services/services.func.js
+++ src/app/home/sections/3-services/services.func.js
@@ -1,19 +1,23 @@
-
-import scrollTrigger from '@/libs/vz/scrollInteraction/scrollTrigger';
-import styles from './services.module.css'
+import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
+import styles from './services.module.css'
+import scrollTrigger from '@/libs/vz/scrollInteraction/scrollTrigger';
+import { dimensionsStore } from '@/utils/store/store';
 
 export function servicesSI() {
+    const pathname = usePathname();
+    const { isMobile } = dimensionsStore();
 
     useEffect(() => {
-            const cards = Array.from(document.getElementsByClassName(styles.serviceCard))
+        if (pathname === '/' || (pathname === '/services' && isMobile())) {
+            const cards = Array.from(document.getElementsByClassName(styles.serviceCard));
             cards.forEach((element, index) => {
                 scrollTrigger.createInstance('service-card' + index, element, {
                     values: [[100, 0]],
                     startPoint: [0],
                     endPoint: [0.9],
-                    callback: ({ v, vw }) => {
-                        element.style.transform = `translate3d(${v[0]}%, 0, 0)`
+                    callback: ({ v }) => {
+                        element.style.transform = `translate3d(${v[0]}%, 0, 0)`;
                     }
                 });
             });
@@ -21,5 +25,6 @@ export function servicesSI() {
             return () => {
                 cards.forEach((_, index) => scrollTrigger.removeInstance('service-card' + index));
             };
-    }, [])
+        }
+    }, [pathname]);
 }

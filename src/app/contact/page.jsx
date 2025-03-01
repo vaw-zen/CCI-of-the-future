4550c44e0ef18faf2746838f3ac7bb727eda5b28
ChaabaneFares <chaaben.fares94@gmail.com>
2025-03-02 00:28:25 +0100

    grid system achived

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,6 +6,27 @@ export default function ContactPage() {
   return (
     <>
       <HeroHeader title="Contact Us" />
+      <div style={{display:"flex",flexDirection:"column",padding: "0 15.975vw",gap:"84px" }}>
+     
+        <div style={{display:"flex",height:"204px",width:"100%"}}  >
+        <div style={{flex:"1", background:"red"}}></div>
+        <div style={{flex:1, background:"blue"}}></div>
+        </div>
+        <div style={{display:"flex",height:"328px",width:"100%"}}  >
+        <div style={{flex:"1", background:"white"}}></div>
+        <div style={{display:"flex",flex:1, background:"green",width:"100%",padding:"1vw 0"}}>
+          <div  style={{display:"flex",flexDirection:"column", flex:"1", background:"black"}}>
+          <div style={{flex:"1", background:"white"}}></div>
+          <div style={{flex:1, background:"blue"}}></div>
+          </div>
+          <div style={{display:"flex",flexDirection:"column",flex:"1", background:"yellow"}}>
+          <div style={{flex:1, background:"red"}}></div>
+          <div style={{flex:1, background:"#cafb42"}}></div>
+          </div>
+        </div>
+        </div>
+    
+      </div>
      
     </>
   )

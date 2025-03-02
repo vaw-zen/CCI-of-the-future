72226053538c11737c8a4560fea2519bb808a56e
ChaabaneFares <chaaben.fares94@gmail.com>
2025-03-02 22:00:38 +0100

    optimizing json content for contact sections and reusable welcoming card

---
+++ src/app/contact/2-welcoming/welcomeCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./welcome.module.css";
+
+const WelcomeCard = ({ imageSrc, title, name, position, email }) => {
+  return (
+    <div className={styles.welcomeCard}>
+      <img className={styles.image} src={imageSrc} alt="welcoming image" />
+      <div className={styles.welcomeinfotext}>
+        <h3>{title}</h3>
+        <div className={styles.nameandposition}>
+          <h5>{name}</h5>
+          <div className={styles.slash}>/</div>
+          <h6>{position}</h6>
+        </div>
+        <h4>{email}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeCard;

f48dce4666694445c2b31e990c1d57bb7bd31ebe
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-03-07 23:31:26 +0100

    contact page : px to vw

--- src/app/contact/2-welcoming/welcomeCard.jsx
+++ src/app/contact/2-welcoming/welcomeCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./welcomeCard.module.css";
+import styles from "./welcome.module.css";
 
 const WelcomeCard = ({ imageSrc, title, name, position, email }) => {
   return (

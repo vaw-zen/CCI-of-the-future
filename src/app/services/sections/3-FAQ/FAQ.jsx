c555fce882192db95ec664ad53bd64ed20d3c595
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-03-09 20:49:13 +0100

    services - faq : ssr + css cleanup

--- src/app/services/sections/3-FAQ/FAQ.jsx
+++ src/app/services/sections/3-FAQ/FAQ.jsx
@@ -1,93 +1,21 @@
-'use client'
 import ResponsiveImage from '@/utils/components/Image/Image'
 import content from './FAQ.json'
-import { useState, useRef, useEffect } from 'react'
-import { IconoirArrowUpRight } from '@/utils/components/icons'
+import styles from './FAQ.module.css'
+import FAQQuestions from './CSR/FAQClient'
 
 export default function FAQ() {
   const { img, title, slug, QA } = content
-  const [activeIndex, setActiveIndex] = useState(null)
-  const [heights, setHeights] = useState({})
-  const answerRefs = useRef([])
-
-  // Initialize or update refs array when QA changes
-  useEffect(() => {
-    answerRefs.current = answerRefs.current.slice(0, QA.length)
-  }, [QA])
-
-  // Measure heights of all answers
-  useEffect(() => {
-    const newHeights = {}
-
-    answerRefs.current.forEach((ref, index) => {
-      if (ref) {
-        // Store the scroll height (actual content height)
-        newHeights[index] = ref.scrollHeight
-      }
-    })
-
-    setHeights(newHeights)
-  }, [QA]) // Re-measure if QA content changes
-
+  
   return (
-    <section style={{ display: 'flex', gap: '2.60vw', marginTop: '5.99vw' }}>
-      <div style={{
-        flex: 1,
-        display: 'flex', flexDirection: 'column', paddingBottom: '1vw',
-      }}>
-        <abbr style={{
-          color: 'var(--ac-primary)', fontSize: '0.83vw', textTransform: 'uppercase',
-          marginBottom: '0.52vw',
-        }}>
-          {slug}
-        </abbr>
-        <strong style={{ fontSize: '3.33vw', marginBottom: '1.04vw' }}>
-          {title}
-        </strong>
-
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '1.56vw' }}>
-          {QA.map((qa, index) => {
-            return <div key={index} style={{ marginBottom: '1vw' }}>
-              <div
-                onClick={() => setActiveIndex(activeIndex === index ? null : index)}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  borderBottom: '1px solid white',
-                  padding: '1.04vw 0',
-                  cursor: 'pointer', width: '100%'
-                }}
-              >
-                <strong style={{ fontSize: '1.35vw', fontWeight: 700, }}>
-                  {qa.Q}
-                </strong>
-                <IconoirArrowUpRight strokeWidth={2.5} style={{ width: '1vw', height: '1vw', color: 'var(--ac-primary)' }} />
-              </div>
-              <div
-                style={{
-                  fontSize: '0.83vw',
-                  fontWeight: 400,
-                  paddingTop: activeIndex === index ? '1.04vw' : '0',
-                  height: activeIndex === index ? `${heights[index]}px` : '0',
-                  overflow: 'hidden',
-                  transition: 'padding 0.3s ease-in-out, height 0.3s ease-in-out, margin-bottom 0.3s ease-in-out',
-                  marginBottom: activeIndex === index ? '-2vw' : '0'
-                }}
-              >
-                <p
-                  ref={el => answerRefs.current[index] = el}
-                >
-                  {qa.A}
-                </p>
-              </div>
-            </div>
-          })}
-        </div>
+    <section className={styles.faqSection}>
+      <div className={styles.faqContent}>
+        <abbr className={styles.slug}>{slug}</abbr>
+        <strong className={styles.title}>{title}</strong>
+        <FAQQuestions QA={QA} />
       </div>
+
       <ResponsiveImage
-        style={{
-          flex: 1, objectFit: 'cover', objectPosition: 'center'
-        }}
+        className={styles.image}
         src={img}
         sizes={[40, 40, 40]}
         alt='FAQ'

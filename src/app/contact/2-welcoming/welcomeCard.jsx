5d4b8717da9a1a7c00264524cbe53133aeae6e6d
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-03-10 17:55:35 +0100

    contact: fix conflict

--- src/app/contact/2-welcoming/welcomeCard.jsx
+++ src/app/contact/2-welcoming/welcomeCard.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import styles from "./welcome.module.css";
-import ResponsiveImage from "@/utils/components/Image/Image";
 
 const WelcomeCard = ({ imageSrc, title, name, position, email }) => {
   return (
     <div className={styles.welcomeCard}>
-      <ResponsiveImage
-      className={styles.image} src={imageSrc} alt="welcoming image"
-      skeleton
-      sizes={[12, 23, 43]}
-      />
+      <img className={styles.image} src={imageSrc} alt="welcoming image" />
       <div className={styles.welcomeinfotext}>
         <h3>{title}</h3>
         <div className={styles.nameandposition}>

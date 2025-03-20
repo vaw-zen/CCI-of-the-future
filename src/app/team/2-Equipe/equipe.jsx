5d739331ec2d467d0aec443dc2e3f517cd18614f
ChaabaneFares <chaaben.fares94@gmail.com>
2025-03-20 22:51:08 +0100

    enough for today

--- src/app/team/2-Equipe/equipe.jsx
+++ src/app/team/2-Equipe/equipe.jsx
@@ -48,41 +48,53 @@ export default function Equipe() {
         instagram: '#',
       },
     },
+    {
+      name: 'Michael Brown',
+      position: 'Head of Development',
+      image: '/our-team/equippe.jpg',
+      socials: {
+        facebook: '#',
+        twitter: '#',
+        linkedin: '#',
+        instagram: '#',
+      },
+    },
   ];
 
   return (
     <div className={styles.container}>
       <h3>Our Team</h3>
       <h2>Meet the experienced team behind our success.</h2>
-      <div className={styles.cardsContainer}>
-        {cards.map((card, index) => (
-          <div className={styles.card} key={index}>
-            <div className={styles.imageContainer}>
-              <img src={card.image} alt={card.name} />
-            </div>
-            <div className={styles.textContainer}>
-              <div className={styles.textContainerContent}>
-                <div className={styles.textContainerContentTop}>
-                  <h3>{card.name}</h3>
-                  <h4>{card.position}</h4>
-                </div>
+      <div className={styles.scrollWrapper}>
+        <div className={styles.cardsContainer}>
+          {cards.map((card, index) => (
+            <div className={styles.card} key={index}>
+              <div className={styles.imageContainer}>
+                <img src={card.image} alt={card.name} />
+              </div>
+              <div className={styles.textContainer}>
+                <div className={styles.textContainerContent}>
+                  <div className={styles.textContainerContentTop}>
+                    <h3>{card.name}</h3>
+                    <h4>{card.position}</h4>
+                  </div>
 
-                <div className={styles.socials} >
-                  <a href="/">
-                    <LineMdInstagram className={styles.icon} />
-                  </a>
-                  <a href="/">
-                    <LineMdFacebook className={styles.icon} />
-                  </a>
-                  <a href="/">
-                    <LineMdLinkedin className={styles.icon} />
-                  </a>
+                  <div className={styles.socials} >
+                    <a href="/">
+                      <LineMdInstagram className={styles.icon} />
+                    </a>
+                    <a href="/">
+                      <LineMdFacebook className={styles.icon} />
+                    </a>
+                    <a href="/">
+                      <LineMdLinkedin className={styles.icon} />
+                    </a>
+                  </div>
                 </div>
               </div>
-
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <GreenBand className={styles.greenBandWrapper}/>
     </div>

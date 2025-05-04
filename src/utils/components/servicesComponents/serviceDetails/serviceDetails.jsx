9f7fc974391272ab5c8cc8f691135e88ae97d15a
ChaabaneFares <chaaben.fares94@gmail.com>
2025-05-04 18:36:09 +0100

    added servoceDetails component

---
+++ src/utils/components/servicesComponents/serviceDetails/serviceDetails.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './serviceDetails.module.css'
+
+export default function ServiceDetails() {
+  return (
+    <div className={styles.container}>
+        <h2 className={styles.title}>
+            Wiring and installation
+        </h2>
+        <div className={styles.textContainer}>
+            <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+            </p>
+        </div>
+    </div>
+  )
+}

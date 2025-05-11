6814278e1f0b95b3e243b99885e664c8d0cfc5f7
ChaabaneFares <chaaben.fares94@gmail.com>
2025-05-11 19:13:17 +0100

    responsive tuning

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -2,13 +2,15 @@ import HeroHeader from '@/utils/components/reusableHeader/HeroHeader'
 import ServiceDetails from '@/utils/components/servicesComponents/serviceDetails/serviceDetails'
 import PartnerTab from '@/utils/components/servicesComponents/partnerTab/PartnerTab'
 import React from 'react'
+import Feedback from '@/utils/components/servicesComponents/feedbackComponent/feedback'
 
 export default function Page() {
     return (
-        <div>
+        <div style={{background:"#141416"}}>
             <HeroHeader title={'Test'} />
             <ServiceDetails />
             <PartnerTab />
+            <Feedback />
         </div>
     )
 }

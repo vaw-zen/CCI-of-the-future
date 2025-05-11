27f7c251cec3e1c0e6dea5c973ea7676939083ee
ChaabaneFares <chaaben.fares94@gmail.com>
2025-05-11 17:12:02 +0100

    horizontal menu component responsivness

--- src/utils/components/servicesComponents/partnerTab/PartnerTab.jsx
+++ src/utils/components/servicesComponents/partnerTab/PartnerTab.jsx
@@ -7,22 +7,22 @@ const TabData = [
     id: 'mission',
     title: 'Our mission',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
-    image: '/path-to-image.jpg'
+    image: 'https://uploads-ssl.webflow.com/63c6818603ef9ce50c6d563d/63d267daedcc413255de10bf_tab-01.jpg'
   },
   {
     id: 'vision',
     title: 'Our vision',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.',
-    image: '/path-to-image.jpg'
+    image: 'https://uploads-ssl.webflow.com/63c6818603ef9ce50c6d563d/63d267dbdb29fef4a994f1cb_tab-02.jpg'
   },
   {
     id: 'philosophy',
     title: 'Philosophy',
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: '/path-to-image.jpg'
+    image: 'https://uploads-ssl.webflow.com/63c6818603ef9ce50c6d563d/63d267daedcc413255de10bf_tab-01.jpg'
+
   }
 ]
-
 export default function PartnerTab() {
   const [activeTab, setActiveTab] = useState('vision')
   
@@ -45,7 +45,7 @@ export default function PartnerTab() {
           activeTab === tab.id && (
             <div 
               key={tab.id} 
-              className={`${styles.tabPanel} ${i % 2 === 1 ? styles.evenPanel : ''}`}
+              className={`${styles.tabPanel} ${i % 2 === 1 ? '' :styles.evenPanel}`}
             >
               <div className={styles.tabDesc}>
                 <h2 className={styles.tabTitle}>{tab.title}</h2>

3199fce810f68a56e94fbb6c30dfe0805264d1f6
ChaabaneFares <chaaben.fares94@gmail.com>
2025-07-23 19:03:19 +0100

    french

--- src/app/team/1-teamMember/teamMember.jsx
+++ src/app/team/1-teamMember/teamMember.jsx
@@ -1,21 +1,28 @@
 import styles from "./teamMember.module.css";
 import React from "react";
 export default function teamMember() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}></div>
-            <div className={styles.infoContainer
-            } >
-                <h2>Our mission is to build for your comfort</h2>
-                <p>We work to ensure people’s comfort at their home, and to provide the best and the fastest help at fair prices. We stand for quality, safety and credibility, so you could be sure about our work.</p>
-                <div className={styles.memberInfo}>
-                    <div className={styles.memberCard}>
-                        <img src="/our-team/photo.png" alt="" />
-                        <h3>Chaabane Fares Co-Founder</h3>
-                    </div>
-                    <div className={styles.memberAutograph}><img src="/our-team/autograph.png" alt="" /></div>
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}></div>
+      <div className={styles.infoContainer}>
+        <h2>Notre mission est de construire pour votre confort</h2>
+        <p>
+          Nous sommes spécialisés dans la fourniture de services de nettoyage
+          industriel et de tapisserie de premier ordre. Notre équipe, dotée
+          d'une expertise approfondie et de technologies de pointe, s'engage à
+          offrir des solutions de nettoyage inégalées dans une variété
+          d'industries.
+        </p>
+        <div className={styles.memberInfo}>
+          <div className={styles.memberCard}>
+            <img src="/our-team/photo.png" alt="" />
+            <h3>Chaabane Fares Co-Founder</h3>
+          </div>
+          <div className={styles.memberAutograph}>
+            <img src="/our-team/autograph.png" alt="" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }

b25d1434d8940af7772a44362cd04e18fd67547c
ChaabaneFares <chaaben.fares94@gmail.com>
2025-07-23 19:45:31 +0100

    solutions

--- src/app/contact/3-form/form.jsx
+++ src/app/contact/3-form/form.jsx
@@ -4,15 +4,15 @@ import GreenBand from '@/utils/components/GreenBand/GreenBand';
 export default function Form() {
   return (
     <div className={styles.formContainer}>
-      <input className={styles.formGroup} type="text" name="name" placeholder="Your Name" />
-      <input className={styles.formGroup} type="email" name="email" placeholder="Email Address" />
-      <input className={styles.formGroup} type="tel" name="phone" placeholder="Phone Number" />
-      <input className={styles.formGroup} type="text" name="projectType" placeholder="Project Type" />
-      <textarea className={`${styles.formGroup} ${styles.textAreaContainer}`} name="message" placeholder="Your Message"></textarea>
+      <input className={styles.formGroup} type="text" name="name" placeholder="Votre nom" />
+      <input className={styles.formGroup} type="email" name="email" placeholder="Adresse e-mail" />
+      <input className={styles.formGroup} type="tel" name="phone" placeholder="Numéro de téléphone" />
+      <input className={styles.formGroup} type="text" name="projectType" placeholder="Type de projet" />
+      <textarea className={`${styles.formGroup} ${styles.textAreaContainer}`} name="message" placeholder="Votre message"></textarea>
       <label className={styles.checkboxContainer}>
-        <input type="checkbox" name="terms" /> I am bound by the terms of the Service I accept Privacy Policy
+        <input type="checkbox" name="terms" /> J'accepte les conditions du service et la politique de confidentialité
       </label>
-      <button className={styles.submitButton} type="submit">SEND MESSAGE</button>
+      <button className={styles.submitButton} type="submit">ENVOYER</button>
       <GreenBand className={styles.greenBandWrapper}/>
     </div>
   );

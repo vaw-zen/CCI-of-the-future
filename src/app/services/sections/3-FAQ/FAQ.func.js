398ffed9ecb1b467a34ffe457bd1fc735965fd52
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-08-15 20:02:22 +0100

    faq: fix answer calculation

--- src/app/services/sections/3-FAQ/FAQ.func.js
+++ src/app/services/sections/3-FAQ/FAQ.func.js
@@ -1,10 +1,12 @@
+import { dimensionsStore } from '@/utils/store/store'
 import { useState, useRef, useEffect } from 'react'
 
 export function useFAQLogic(QA) {
+  const { isMobile, isTablet, isDesktop, vw } = dimensionsStore()
+
   const [activeIndex, setActiveIndex] = useState(null)
   const [heights, setHeights] = useState({})
   const answerRefs = useRef([])
-
   useEffect(() => {
     answerRefs.current = answerRefs.current.slice(0, QA.length)
   }, [QA])
@@ -12,10 +14,12 @@ export function useFAQLogic(QA) {
   useEffect(() => {
     const newHeights = {}
     answerRefs.current.forEach((ref, index) => {
-      if (ref) newHeights[index] = ref.scrollHeight
+      const parentPadding = isDesktop() ? vw * 0.0104 : isTablet() ? vw * 0.026 : isMobile() ? vw * 0.05 : 0
+
+      if (ref) newHeights[index] = ref.scrollHeight + parentPadding
     })
     setHeights(newHeights)
-  }, [QA])
+  }, [QA, vw])
 
   const toggleQuestion = (index) => {
     setActiveIndex(activeIndex === index ? null : index)

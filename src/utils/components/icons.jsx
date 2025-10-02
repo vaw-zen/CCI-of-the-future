4c7e6b0b79bed0632b7a3b8342d390b22cce5b44
ChaabaneFares <chaaben.fares94@gmail.com>
2025-10-02 19:17:59 +0100

    custom controler

--- src/utils/components/icons.jsx
+++ src/utils/components/icons.jsx
@@ -113,3 +113,25 @@ import React from 'react';
 export function LineMdCalendar(props) {
 	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><rect width={14} height={0} x={5} y={5} fill="currentColor"><animate fill="freeze" attributeName="height" begin="0.6s" dur="0.2s" values="0;3"></animate></rect><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path strokeDasharray={64} strokeDashoffset={64} d="M12 4h7c0.55 0 1 0.45 1 1v14c0 0.55 -0.45 1 -1 1h-14c-0.55 0 -1 -0.45 -1 -1v-14c0 -0.55 0.45 -1 1 -1Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"></animate></path><path strokeDasharray={4} strokeDashoffset={4} d="M7 4v-2M17 4v-2"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.2s" values="4;0"></animate></path><path strokeDasharray={12} strokeDashoffset={12} d="M7 11h10"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.8s" dur="0.2s" values="12;0"></animate></path><path strokeDasharray={8} strokeDashoffset={8} d="M7 15h7"><animate fill="freeze" attributeName="stroke-dashoffset" begin="1s" dur="0.2s" values="8;0"></animate></path></g></svg>);
 }
+
+// Video Control Icons
+export function MdiPause(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M14 19h4V5h-4M6 19h4V5H6z"></path></svg>);
+}
+
+export function LineMdVolumeHighFilled(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><g fill="currentColor" fillOpacity={0}><path d="M5 8.5v7c0 .55.45 1 1 1h2.1l3.9 3.9l3.9-3.9H18c.55 0 1-.45 1-1v-7c0-.55-.45-1-1-1h-2.1l-3.9-3.9l-3.9 3.9H6c-.55 0-1 .45-1 1z"><animate fill="freeze" attributeName="fill-opacity" begin="0.2s" dur="0.15s" values="0;1"></animate></path><path d="M11.5 6.5v11c0 .6-.4 1-1 1s-1-.4-1-1v-11c0-.6.4-1 1-1s1 .4 1 1z"><animate fill="freeze" attributeName="fill-opacity" begin="0.35s" dur="0.15s" values="0;1"></animate></path></g><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}><path strokeDasharray={28} strokeDashoffset={28} d="M6 14.5V9.5c0-.55.45-1 1-1h2l4-4v15l-4-4H7c-.55 0-1-.45-1-1z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="28;0"></animate></path><path strokeDasharray={8} strokeDashoffset={8} d="M15.5 7.5c1.5 1.33 1.5 6.67 0 8"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.5s" dur="0.2s" values="8;0"></animate></path><path strokeDasharray={10} strokeDashoffset={10} d="M17.5 5.5c3 2.5 3 10.5 0 13"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.7s" dur="0.2s" values="10;0"></animate></path></g></svg>);
+}
+
+export function MdiVolumeMute(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 4L9.91 6.09L12 8.18M4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21L21 19.73l-9-9M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.96 8.96 0 0 0 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.42.05-.63"></path></svg>);
+}
+
+export function MdiFullscreen(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M5 5h5V3H3v7h2zm5 14H5v-5H3v7h7zm11-5h-2v5h-5v2h7zm-2-4h2V3h-7v2h5z"></path></svg>);
+}
+
+export function MdiFullscreenExit(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M14 14h5v2h-7v-7h2zm-9 0h2v5h5v2H5v-7zm5-9h7v7h-2V9h-5zM5 5h5v2H8v2H6V7H5z"></path></svg>);
+}
+
